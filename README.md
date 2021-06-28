c720b509b1fcd2ce005686010bfd53225b82fffe
Sanket <sanket9242@gmail.com>
2021-06-28 19:17:20 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1>ac-react-calculator</h1>
+
+A React calculator built with [React.js](https://github.com/facebook/react), [emotion](https://github.com/emotion-js/emotion) and [hooks](https://reactjs.org/docs/hooks-intro.html).</p> I've used [this calculator design concept](https://www.uplabs.com/posts/calculator-animation-icons) which is designed by Maximiliano Catani.
+
+## Live Demo
+
+You can test on https://ac-react-calculator.netlify.com
+
+## Preview
+
+![Preview](screenshot.jpg)
+
+## Running Locally
+
+1. Clone this repo
+2. Type `cd ac-react-calculator` to enter the project folder
+3. Run `npm install` and install dependencies
+4. Run `npm start` and visit [localhost:3000](http://localhost:3000)
+
+## Build
+
+1. Run `npm run build`
+1. The compiled version will be in `/build/`
+
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+## Eject
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,153 @@
+/* eslint-disable no-eval */
+import React, { useState } from 'react';
+import Screen from '../Screen';
+import LastOperation from '../LastOperation';
+import Button from '../Button';
+import {
+  CalculatorWrapper,
+  ButtonList,
+  Row,
+  Left,
+  Right,
+  ButtonContainer,
+} from './Calculator.styles';
+
+const Calculator = () => {
+  const [lastOperation, setLastOperation] = useState('');
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [operatorFlag, setOperatorFlag] = useState(false);
+  const [decimalFlag, setDecimalFlag] = useState(false);
+
+  const getButtonValues = (type = 'left') => {
+    const colors = {
+      orange: '#f68966',
+      orangeLinear: 'linear-gradient(to bottom, #f68966, #fd6c1b)',
+      pink: '#f43163',
+      pinkLinear: 'linear-gradient(to bottom, #f43163, #fb0f4c)',
+      red: '#fb155D',
+    };
+
+    const values = {
+      top: [
+        { id: 'clear', value: 'C', isOval: true, backgroundColor: '#fff', textColor: colors.red },
+        { id: 'percentage', value: '%', isOval: true, backgroundColor: colors.orange, backgroundImage: colors.orangeLinear },
+        { id: 'divide', value: '/', isOval: true, backgroundColor: colors.orange, backgroundImage: colors.orangeLinear },
+        { id: 'multiply', value: '*', display: 'x', isOval: true, backgroundColor: colors.orange, backgroundImage: colors.orangeLinear },
+      ],
+      left: [
+        { id: 'one', value: '1' },
+        { id: 'two', value: '2' },
+        { id: 'three', value: '3' },
+        { id: 'four', value: '4' },
+        { id: 'five', value: '5' },
+        { id: 'six', value: '6' },
+        { id: 'seven', value: '7' },
+        { id: 'eight', value: '8' },
+        { id: 'nine', value: '9' },
+        { id: 'remove', value: '←', isOval: true, backgroundColor: '#34386A' },
+        { id: 'zero', value: '0' },
+        { id: 'decimal', value: '.' },
+      ],
+      right: [
+        { id: 'add', value: '+', isOval: true, backgroundColor: colors.orange, backgroundImage: colors.orangeLinear },
+        { id: 'subtract', value: '-', isOval: true, backgroundColor: colors.orange, backgroundImage: colors.orangeLinear },
+        { id: 'equals', value: '=', isLong: true, backgroundColor: colors.pink, backgroundImage: colors.pinkLinear },
+      ],
+    };
+
+    return values[type];
+  }
+
+  const handleButtonClick = (buttonValue) => {
+    switch (buttonValue) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9': {
+        if (currentNumber !== '0') {
+          setCurrentNumber(currentNumber + buttonValue);
+          setOperatorFlag(false);
+        } else {
+          setCurrentNumber(buttonValue);
+        }
+        break;
+      }
+      case '*':
+      case '+':
+      case '-':
+      case '/':
+      case '%': {
+        if (!operatorFlag) {
+          setCurrentNumber(currentNumber + buttonValue);
+          setOperatorFlag(buttonValue);
+        } else {
+          const newNumber = currentNumber.slice(0, currentNumber.length - 1);
+          setCurrentNumber(newNumber + buttonValue);
+        }
+        break;
+      }
+      case 'C':
+        setLastOperation('');
+        setCurrentNumber('0');
+        break;
+      case '=':
+        setLastOperation(currentNumber);
+        const result = currentNumber.includes('.') ? eval(currentNumber).toFixed(2) : eval(currentNumber);
+        setCurrentNumber(result);
+
+        break;
+      case '.':
+        if (!decimalFlag) {
+          setCurrentNumber(currentNumber + '.');
+          setDecimalFlag(true);
+        }
+        break;
+      case '←':
+        const newNumber = currentNumber.length > 1 ? currentNumber.slice(0, currentNumber.length - 1) : '0';
+        setCurrentNumber(newNumber);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return (
+    <CalculatorWrapper>
+      <LastOperation operation={lastOperation} />
+      <Screen currentNumber={currentNumber} isAnimated={false} />
+      <ButtonList>
+        <Row>
+          <ButtonContainer>
+            {getButtonValues('top').map(b => (
+              <Button key={b.id} {...b} onClick={() => handleButtonClick(b.value)} />
+            ))}
+          </ButtonContainer>
+        </Row>
+        <Row>
+          <Left>
+            <ButtonContainer>
+              {getButtonValues('left').map(b => (
+                <Button key={b.id} {...b} onClick={() => handleButtonClick(b.value)} />
+              ))}
+            </ButtonContainer>
+          </Left>
+          <Right marginLeft="11px">
+            <ButtonContainer>
+              {getButtonValues('right').map(b => (
+                <Button key={b.id} {...b} onClick={() => handleButtonClick(b.value)} noMargin />
+              ))}
+            </ButtonContainer>
+          </Right>
+        </Row>
+      </ButtonList>
+    </CalculatorWrapper>
+  );
+}
+
+export default Calculator;
